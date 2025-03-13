34cac945949dc4cfd474962c0c56cc73ee3ab789
retessam7 <retessam7@gmail.com>
2025-03-13 21:16:28 +0100

    iOS keychain

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -13,10 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Nothing to see here"
+        
         // from project 19
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        // when our app has been backgrounded or the user has switched to multitasking mode
+        // calls our saveSecretMessage() directly when the notification comes in, which means the app automatically saves any text and hides it when the app is moving to a background state.
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
     }
 
     @objc func adjustForKeyboard(notification: Notification) {
@@ -36,7 +42,31 @@ class ViewController: UIViewController {
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
     }
+    
     @IBAction func authenticateTapped(_ sender: Any) {
+        unlockSecretMessage()
+    }
+    
+    // needs to show the text view, then load the keychain's text into it
+    func unlockSecretMessage() {
+        secret.isHidden = false
+        title = "Secret stuff!"
+        
+        //  Loading strings from the keychain using
+        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
+            secret.text = text
+        }
+    }
+    
+    // to write the text view's text to the keychain, then make it hidden
+    @objc func saveSecretMessage() {
+        guard secret.isHidden == false else { return }
+        
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        // is used to tell a view that has input focus that it should give up that focus (to tell our text view that we're finished editing it, so the keyboard can be hidden)
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "Nothing to see here"
     }
 }
 
