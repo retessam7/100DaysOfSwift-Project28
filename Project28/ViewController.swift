e96f25a9bc8a44b6c925904ff6a97c1f23feb2d2
retessam7 <retessam7@gmail.com>
2025-03-13 18:07:21 +0100

    Initial Commit

---
+++ Project28/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project28
+//
+//  Created by Aleksei Ivanov on 13/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
